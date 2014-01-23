8ccdd58f1639020be92dbaaac2beac9bcf11526e
Jeremy Ward <jrmy.ward@gmail.com>
2014-01-23 13:51:41 -0800

    Updated default attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 node.default['reviewitician']['app_name']     = 'reviewitician'
 node.default['reviewitician']['environment']  = 'production'
 node.default['reviewitician']['db_name']      = "#{node["reviewitician"]["app_name"]}_#{node["reviewitician"]["environment"]}"
-node.default['reviewitician']['db_password']  = '$1$r0jdbv/X$I1boMWS9ln6X0lD9n6gtm1'
+node.default['reviewitician']['db_password']  = 'Revi3wMypAssw0rdnoW'
